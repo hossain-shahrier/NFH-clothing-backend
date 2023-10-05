f9e91c06bedd79887244d0ae5fdc34caabd766f0
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-10-06 00:21:46 +0600

    Customer controller and Model

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class CustomerController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $customers = Customer::all();
+
+        return response()->json($customers);
+    }
+
+    public function show(Customer $customer): JsonResponse
+    {
+        return response()->json($customer);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $customer = new Customer();
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->phone = $request->input('phone');
+        $customer->address = $request->input('address');
+        $customer->save();
+
+        return response()->json($customer);
+    }
+
+    public function update(Request $request, Customer $customer): JsonResponse
+    {
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->phone = $request->input('phone');
+        $customer->address = $request->input('address');
+        $customer->save();
+
+        return response()->json($customer);
+    }
+
+    public function destroy(Customer $customer): JsonResponse
+    {
+        $customer->delete();
+
+        return response()->json();
+    }
 }
