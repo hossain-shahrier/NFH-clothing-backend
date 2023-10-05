5e77a8e15bd3b51b5397c4d2e8f1d99b2ea66077
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-10-06 00:23:53 +0600

    Order controller and Model

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class OrderController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $orders = Order::all();
+
+        return response()->json($orders);
+    }
+
+    public function show(Order $order): JsonResponse
+    {
+        return response()->json($order);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $order = new Order();
+        $order->customer_id = $request->input('customer_id');
+        $order->product_id = $request->input('product_id');
+        $order->quantity = $request->input('quantity');
+        $order->status = $request->input('status');
+        $order->total_amount = $request->input('total_amount');
+        $order->save();
+
+        return response()->json($order);
+    }
+
+    public function update(Request $request, Order $order): JsonResponse
+    {
+        $order->customer_id = $request->input('customer_id');
+        $order->product_id = $request->input('product_id');
+        $order->quantity = $request->input('quantity');
+        $order->status = $request->input('status');
+        $order->total_amount = $request->input('total_amount');
+        $order->save();
+
+        return response()->json($order);
+    }
+
+    public function destroy(Order $order): JsonResponse
+    {
+        $order->delete();
+
+        return response()->json();
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'quantity',
+        'status',
+        'total_amount',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
