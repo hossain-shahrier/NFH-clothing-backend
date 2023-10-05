aacc9bacccd9985d5b33dd9aa542db57562d2390
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-10-06 00:25:54 +0600

    Post controller and Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $posts = Post::all();
+
+        return response()->json($posts);
+    }
+
+    public function show(Post $post): JsonResponse
+    {
+        return response()->json($post);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->caption = $request->input('caption');
+        $post->image_paths = $request->input('image_paths');
+        $post->video_paths = $request->input('video_paths');
+        $post->product_id = $request->input('product_id');
+        $post->save();
+
+        return response()->json($post);
+    }
+
+    public function update(Request $request, Post $post): JsonResponse
+    {
+        $post->title = $request->input('title');
+        $post->caption = $request->input('caption');
+        $post->image_paths = $request->input('image_paths');
+        $post->video_paths = $request->input('video_paths');
+        $post->product_id = $request->input('product_id');
+        $post->save();
+
+        return response()->json($post);
+    }
+
+    public function destroy(Post $post): JsonResponse
+    {
+        $post->delete();
+
+        return response()->json();
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'caption',
+        'image_paths',
+        'video_paths',
+        'product_id',
+    ];
+
+    protected $casts = [
+        'image_paths' => 'json',
+        'video_paths' => 'json',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
