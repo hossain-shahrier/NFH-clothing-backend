89c9a3086993cee02d22b0087db21eed9acd42d3
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2023-10-06 00:17:30 +0600

    Category controller and Model

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $categories = Category::all();
+
+        return response()->json($categories);
+    }
+
+    public function show(Category $category): JsonResponse
+    {
+        return response()->json($category);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $category = new Category();
+        $category->name = $request->input('name');
+        $category->description = $request->input('description');
+        $category->save();
+
+        return response()->json($category);
+    }
+
+    public function update(Request $request, Category $category): JsonResponse
+    {
+        $category->name = $request->input('name');
+        $category->description = $request->input('description');
+        $category->save();
+
+        return response()->json($category);
+    }
+
+    public function destroy(Category $category): JsonResponse
+    {
+        $category->delete();
+
+        return response()->json();
+    }
 }
